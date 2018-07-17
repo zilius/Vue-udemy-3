b7949e7ade08548043c997fa2a63f6ff8918cba1
DESKTOP-OOTFE5Q\Zygimantas <zygimantas.zilevicius@gmail.com>
2018-07-17 23:15:03 +0300

    files

---
+++ src/components/Signin.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="from-inline">
+        <h3>Sign In</h3>
+        <div class="form-group">
+
+            <input 
+                type="text"
+                placeholder="email"
+                class="form-control"
+                v-model="email"
+            />
+            <input
+                type="password"
+                placeholder="password"
+                class="form-control"
+                v-model="password"
+            />
+
+            <br>
+            <button class="btn btn-primary" @click="signIn">Sign In</button>
+        </div>
+        <br>
+        <p>{{error.message}}</p>
+        <router-link to="/signup">Don't have a acoount? Sign up</router-link>
+    </div>
+</template>
+
+<script>
+
+import { firebaseApp } from '../firebaseApp'
+
+export default {
+    data() {
+        return {
+            email : '',
+            password : '',
+            error: {
+                message: ''
+            }
+            
+        }
+    },
+    methods: {
+        signIn() {
+            firebaseApp.auth().signInWithEmailAndPassword(this.email,this.password)
+                .catch(error => {
+                    this.error = error
+                })
+        }
+    }    
+}
+</script>
+
